6bb6b3c7d6f08f9601dc8b6293fc7154cd9e4a62
CoolPolishGuy <wojciech.myszkorowski@heig-vd.ch>
2017-10-19 00:15:07 +0200

    eslint pd

--- apache-php-image/content/js/chart.js
+++ apache-php-image/content/js/chart.js
@@ -1,27 +1,21 @@
 function buildChart(object) {
-let ctx = document.getElementById("bar-chart");
-let ctx2 = document.getElementById("pie-chart");
+  const ctx = document.getElementById("bar-chart");
+  const ctx2 = document.getElementById("pie-chart");
 
 
-const Personnallabels = [];
-const Personnaldata = object.stats.daysOfWeek;
-const Personnaldata2 = object.stats.trimester;
+  const Personnaldata = object.stats.daysOfWeek;
+  const Personnaldata2 = object.stats.trimester;
 
-//object.forEach((element) => {
 
-    //Personnallabels.push(element.name);
-    //Personnaldata.push(element.commits);
-  
-//});
-let graph = new Chart(ctx, {
+  const graph = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+      labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
       datasets: [
         {
           label: "commits (unite)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#c45850","#c45850"],
-          data: Personnaldata
+          backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850', '#c45850','#c45850'],
+          data: Personnaldata,
         }
       ]
     },
@@ -30,18 +24,18 @@ let graph = new Chart(ctx, {
       responsive: false,
     }
 });
-let myPieChart = new Chart(ctx2,{
-  type: 'pie',
-  data: {
+  const myPieChart = new Chart(ctx2,{
+    type: 'pie',
+    data: {
       datasets: [{
-          data: Personnaldata2,
-          backgroundColor: [
-            'rgba(255, 0, 0, 0.3)',
-            'rgba(0, 255, 0, 0.3)',
-            'rgba(0, 0, 255, 0.3)',
-            'rgba(255, 128, 0, 0.3)',
-          ],
-          label: 'Dataset 1'
+        data: Personnaldata2,
+        backgroundColor: [
+          'rgba(255, 0, 0, 0.3)',
+          'rgba(0, 255, 0, 0.3)',
+          'rgba(0, 0, 255, 0.3)',
+          'rgba(255, 128, 0, 0.3)',
+        ],
+        label: 'Dataset 1',
       }],
       labels: [
         '1st trimester',
@@ -49,10 +43,10 @@ let myPieChart = new Chart(ctx2,{
         '3rd trimester',
         '4th trimester',
       ]
-  },
-  options: {
+    },
+    options: {
       responsive: false,
-  }
+    }
 });
 
 }
--- apache-php-image/content/js/dataSend.js
+++ apache-php-image/content/js/dataSend.js
@@ -1,23 +1,17 @@
 $(document).ready(() => {
   const req = new XMLHttpRequest();
 
-
   req.open('GET','https://raw.githubusercontent.com/CoolPolishGuy/TWEB-GitHub-Analytics-Agent/master/data/data.json');
   req.responseType = 'json';
- 
 
   req.onreadystatechange = () => {
-    if(req.readyState === XMLHttpRequest.DONE &&req.status === 200) {
-      
+    if (req.readyState === XMLHttpRequest.DONE &&req.status === 200) {
       const object = req.response;
-      for(let i = 0; i < object.repoCommits.length; i++) {
-        $("table").append(`<tr><td id="repList"> ${object.repoCommits[i].name} </td><td id="reIssues">${object.repoCommits[i].commits} </td></tr>`);
- 
-        //buildChart(object);
-        
-    }
+      for (let i = 0; i < object.repoCommits.length; i = +1) {
+        $('table').append(`<tr><td id="repList"> ${object.repoCommits[i].name} </td><td id="reIssues">${object.repoCommits[i].commits} </td></tr>`);     
+      }
     buildChart(object);
-  }
+    }
   };
   req.send();
 });
