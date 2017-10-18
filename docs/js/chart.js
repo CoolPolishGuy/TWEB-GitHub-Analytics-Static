2ca44b645a10ace4cbc1f3d795d320fb619fa720
CoolPolishGuy <wojciech.myszkorowski@heig-vd.ch>
2017-10-19 00:20:02 +0200

    merge

---
+++ docs/js/chart.js
@@ -0,0 +1,52 @@
+function buildChart(object) {
+  const ctx = document.getElementById("bar-chart");
+  const ctx2 = document.getElementById("pie-chart");
+
+
+  const Personnaldata = object.stats.daysOfWeek;
+  const Personnaldata2 = object.stats.trimester;
+
+
+  const graph = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      datasets: [
+        {
+          label: "commits (unite)",
+          backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850', '#c45850','#c45850'],
+          data: Personnaldata,
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      responsive: false,
+    }
+});
+  const myPieChart = new Chart(ctx2,{
+    type: 'pie',
+    data: {
+      datasets: [{
+        data: Personnaldata2,
+        backgroundColor: [
+          'rgba(255, 0, 0, 0.3)',
+          'rgba(0, 255, 0, 0.3)',
+          'rgba(0, 0, 255, 0.3)',
+          'rgba(255, 128, 0, 0.3)',
+        ],
+        label: 'Dataset 1',
+      }],
+      labels: [
+        '1st trimester',
+        '2nd trimester',
+        '3rd trimester',
+        '4th trimester',
+      ]
+    },
+    options: {
+      responsive: false,
+    }
+});
+
+}
--- docs/js/dataSend.js
+++ docs/js/dataSend.js
@@ -1,17 +1,17 @@
-$(function() {
-    console.log("loading data");
+$(document).ready(() => {
+  const req = new XMLHttpRequest();
 
-    function loadData() {
-      $.getJSON("./file.json", function (data){
-        console.log(data);
+  req.open('GET','https://raw.githubusercontent.com/CoolPolishGuy/TWEB-GitHub-Analytics-Agent/master/data/data.json');
+  req.responseType = 'json';
 
-        if(data.length > 0) {
-
-        }
-        //$(".section-heading").text(message);
-        
-      });
-    };
-    loadData();
-    setInterval(loadData, 2000);
+  req.onreadystatechange = () => {
+    if (req.readyState === XMLHttpRequest.DONE &&req.status === 200) {
+      const object = req.response;
+      for (let i = 0; i < object.repoCommits.length; i = +1) {
+        $('table').append(`<tr><td id="repList"> ${object.repoCommits[i].name} </td><td id="reIssues">${object.repoCommits[i].commits} </td></tr>`);     
+      }
+    buildChart(object);
+    }
+  };
+  req.send();
 });
